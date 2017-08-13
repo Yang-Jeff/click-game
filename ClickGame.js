f6f32d5e18a3eb53a0c6e90763c7fa51c3ff2cb5
Jeff Yang <jeffjieyang@gmail.com>
2017-08-13 17:35:23 -0600

    Add start button, some refactoring and bug fixes
    Added a start button, allows for repeat games without refreshing the
    page now. Moved some repeat code into functions, fixed a small bug
    with "targets left" display where it would not update properly.

--- ClickGame.js
+++ ClickGame.js
@@ -4,35 +4,23 @@ var context = canvas.getContext("2d");
 var circX, circY; // center coordinates for the circle
 var clickX, clickY; // coordinates of the user click event
 const TARGET_RAD = 20; // radius of the target circle
+const TOTAL_TARGETS = 10
 
 var canvasLeft = canvas.offsetLeft;
 var canvasTop = canvas.offsetTop;
 
-var numTargetLeft = 10;  // number of targets the user needs to hit
+var numTargetLeft = TOTAL_TARGETS;  // number of targets the user needs to hit
 var targetLeftText = document.getElementById("targetsLeft");
 
 var startTime;
 var endTime;
-var timeTextSec = document.getElementById("timeSec");
-var timeTextMilliSec = document.getElementById("timeMilliSec");
 
-
-var start = false;  // has the game started?
-
+var gameStart = false;  // has the game started?
 
 targetLeftText.innerHTML = numTargetLeft;
 
-
-// this code generates the first circle
-// TODO sometimes the circle is generated outside of canvas, need to fix
-circX = Math.floor((Math.random() * canvas.width - (2 * TARGET_RAD)) + TARGET_RAD);
-circY = Math.floor((Math.random() * canvas.height - (2 * TARGET_RAD)) + TARGET_RAD);
-context.beginPath();
-context.arc(circX,circY,TARGET_RAD,0,2*Math.PI);
-context.fillStyle = "#FF0000";
-context.fill();
-context.stroke();
-
+// draw start button
+drawStartButton();
 
 console.log("About to start");
 // listening for user click
@@ -44,51 +32,88 @@ canvas.addEventListener('click', function(event){
 
 
 
+// functions below
 
 // main game logic
 function clickEvent(event){
-  if (start == false){  // if the game has not started, start it
-    start = true;
-    startTime = Date.now();
-    console.log("Started");
-  }
-  // because of how onclick works this has to be 1. stop the game once user has clicked all targets
-  if (numTargetLeft == 1){
-    endTime = Date.now();
-    var gameTime = endTime - startTime;
-
-    timeTextSec.innerHTML = Math.floor(gameTime / 1000);  // display seconds
-    timeTextMilliSec.innerHTML = gameTime % 1000; // display milliseconds
-    document.getElementById("timePeriod").removeAttribute("hidden");
-    document.getElementById("timeUnits").removeAttribute("hidden");
-
-    // TODO this is not working, how to remove listener once game is done???
-    context.clearRect(0,0,canvas.width,canvas.height);
-    this.removeEventListener('click', function(event){}, false);
-  }
-
-  console.log("In click event")
 
   clickX = event.pageX - canvasLeft;
   clickY = event.pageY - canvasTop;
 
-  if (Math.pow((clickX - circX), 2) + Math.pow((clickY - circY), 2) <= 400 ){
+  if (gameStart == false){  // if the game has not started, start it
+
+    if (clickX >= canvas.width/2 - 50 && clickX <= canvas.width/2 + 50 && clickY >= canvas.height/2 - 35 && clickY <= canvas.height/2 + 15){
+      context.clearRect(0,0,canvas.width,canvas.height);
+      gameStart = true;
+      gameEnd = false;
+      startTime = Date.now();
+      console.log("Started");
+
+      drawCircleTarget();
+
+      targetLeftText.innerHTML = numTargetLeft;
+    }
+
+  }
+  // because of how onclick works this has to be 1. stop the game once user has clicked all targets
+  if (numTargetLeft == 1){
+
+    console.log("Game end");
+    // TODO why is final circle not cleared
+    context.clearRect(0,0,canvas.width,canvas.height);
+    targetLeftText.innerHTML = 0;
+
+    endTime = Date.now();
+    var gameTime = endTime - startTime;
+
+    // reset game values
+    gameStart = false;
+    numTargetLeft = TOTAL_TARGETS;
+
+    // display time taken
+    context.font = "30px Helvetica Neue";
+    context.textAlign = "center";
+    context.fillStyle = "#212121";
+    context.fillText("Your time is: " + Math.floor(gameTime/1000) + "." + gameTime%1000 + " seconds",
+        canvas.width/2, canvas.height/2 - 100);
+
+    drawStartButton();
+  }
+
+
+  if ( gameStart == true && Math.pow((clickX - circX), 2) + Math.pow((clickY - circY), 2) <= 400 ){
 
     numTargetLeft--;
-
     targetLeftText.innerHTML = numTargetLeft;
 
     context.clearRect(0,0,canvas.width,canvas.height);
 
-    circX = Math.floor((Math.random() * canvas.width - (2 * TARGET_RAD)) + TARGET_RAD);
-    circY = Math.floor((Math.random() * canvas.height - (2 * TARGET_RAD)) + TARGET_RAD);
-
-    context.beginPath();
-    context.arc(circX,circY,TARGET_RAD,0,2*Math.PI);
-    context.fillStyle = "#FF0000";
-    context.fill();
-    context.stroke();
-
+    drawCircleTarget();
   }
 
+}
+
+// this code draws the start button
+function drawStartButton(){
+  context.fillStyle = "#FF0000";
+  context.rect(canvas.width/2 - 50, canvas.height/2 - 35, 100, 50);
+  context.fill();
+  context.stroke();
+
+  context.fillStyle = "#212121";
+  context.font = "30px Helvetica Neue";
+  context.textAlign = "center";
+  context.fillText("Start", canvas.width/2, canvas.height/2);
+}
+
+// this function draws a target circle at a random location
+// TODO sometimes the circle is generated outside of canvas, need to fix
+function drawCircleTarget(){
+  circX = Math.floor((Math.random() * canvas.width - (2 * TARGET_RAD)) + TARGET_RAD);
+  circY = Math.floor((Math.random() * canvas.height - (2 * TARGET_RAD)) + TARGET_RAD);
+  context.beginPath();
+  context.arc(circX,circY,TARGET_RAD,0,2*Math.PI);
+  context.fillStyle = "#FF0000";
+  context.fill();
+  context.stroke();
 }
