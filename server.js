02618f53b61724347d274787f17fcb0a3ba9e0a2
Jeff Yang <jeffjieyang@gmail.com>
2017-08-26 22:01:32 -0700

    Add foundation for Nodejs server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+var path = require('path');
+
+var port = 3000;
+
+app.listen(port, function(){
+    console.log("Express app listening on port " + port);
+});
+
+// TODO add compression???
+app.use(express.static('./public'));
