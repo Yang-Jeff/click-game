4c4e7215c02db4604902f36a287342434061c7aa
Jeff Yang <jeffjieyang@gmail.com>
2017-09-01 11:58:45 -0700

    Add sqlite database file and GET request for scores
    Created a Scores.db file with a number of sample entires, added a
    GET method to server.js to retrieve the scores.

--- server.js
+++ server.js
@@ -3,9 +3,42 @@ const app = express();
 
 var port = 3000;
 
+var sqlite3 = require('sqlite3').verbose(); // why verbose
+var db = new sqlite3.Database('Scores.db');
+
+/*
+, sqlite3.OPEN_CREATE,// TODO idk about OPEN_CREATE here
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Database connected");
+  }
+);
+*/
+// TODO make sure scores table is here and ready to go
+
+
+
+
 app.listen(port, function(){
     console.log("Express app listening on port " + port);
 });
 
 // TODO add compression???
-app.use(express.static('./public'));
+app.use(express.static('./public'));  // this sends clientside files
+
+// TODO change score name?
+// this posts new score to server
+app.post('/score', function(request, response){
+  db.run("INSERT INTO ")
+});
+
+// this retrieves the high scores
+app.get('/highscores', function(request, response){
+  db.all("SELECT * FROM Scores", function(err, rows){
+
+    console.log("GET request for scores");
+    response.send(rows);
+  });
+})
