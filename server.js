1eb69784a4634e2fabf659a1ff07a06b35732e79
Jeff Yang <jeffjieyang@gmail.com>
2017-09-09 12:12:53 -0700

    Add comment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var db = new sqlite3.Database('Scores.db');
 app.use(bodyParser.json());
 
 
-
+// setting up server
 app.listen(port, function(){
     console.log("Express app listening on port " + port);
 });
